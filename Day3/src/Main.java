978a4fcddae62d49ef03ad2f7c9ceabcb9338706
Frederik <frederik290@gmail.com>
2019-12-31 14:36:31 +0100

    Removed redundant code

--- Day3/src/Main.java
+++ Day3/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         String inputPath = Paths.get(Paths.get("").toAbsolutePath() + "/Day3/input.txt").toString();
         String[] lines = Util.getLinesFromFile(inputPath);
-        String[] p1 = Arrays.stream(lines[0].split(",")).toArray(String[]::new);
-        String[] p2 =  Arrays.stream(lines[1].split(",")).toArray(String[]::new);
+        String[] p1 = lines[0].split(",");
+        String[] p2 =  lines[1].split(",");
 
         CrossedWires cw = new CrossedWires();
 //        cw.getClosestIntersectionDistanceByManhattanDistance(p1, p2);
