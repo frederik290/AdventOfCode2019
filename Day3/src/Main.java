87b46ca54b62aeb4f3cd382c1a36704c023aebca
Frederik <frederik290@gmail.com>
2019-12-31 14:24:51 +0100

    Changed to relative input paths

--- Day3/src/Main.java
+++ Day3/src/Main.java
@@ -1,8 +1,10 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String inputPath = "/Users/frederik290/IdeaProjects/AdventOfCode2019/Day3/input.txt";
+        String inputPath = Paths.get(Paths.get("").toAbsolutePath() + "/Day3/input.txt").toString();
         String[] lines = Util.getLinesFromFile(inputPath);
         String[] p1 = Arrays.stream(lines[0].split(",")).toArray(String[]::new);
         String[] p2 =  Arrays.stream(lines[1].split(",")).toArray(String[]::new);
@@ -10,5 +12,6 @@ public class Main {
         CrossedWires cw = new CrossedWires();
 //        cw.getClosestIntersectionDistanceByManhattanDistance(p1, p2);
         cw.getClosestIntersectionDistanceByMinimumSteps(p1, p2);
+
     }
 }
